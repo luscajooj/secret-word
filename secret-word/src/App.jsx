78324b51097de097d2e478a831dc23205184eedf
Lucas Ramos <lucas.ramos.guimaraes@gmail.com>
2025-07-18 15:16:51 -0300

    Gameover logic added

--- secret-word/src/App.jsx
+++ secret-word/src/App.jsx
@@ -17,6 +17,8 @@ const stages = [
   { id: 3, name: "end" },
 ];
 
+const guessesQty = 3;
+
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
@@ -27,7 +29,7 @@ function App() {
 
   const [guessedLetters, setGuessedLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
-  const [guesses, setGuesses] = useState(3);
+  const [guesses, setGuesses] = useState(guessesQty);
   const [score, setScore] = useState(0);
 
   const pickWordAndCategory = () => {
@@ -91,14 +93,30 @@ function App() {
         ...actualWrongLetters,
         normalizedLetter,
       ]);
-    }
 
+      setGuesses((actualGuesses) => actualGuesses - 1);
+    }
   };
-  console.log(guessedLetters);
-  console.log(wrongLetters);
-  
+
+  const clearLetterStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  };
+
+  useEffect(() => {
+    if (guesses <= 0) {
+      // Reset all states
+      clearLetterStates();
+
+      setGameStage(stages[2].name);
+    }
+  }, [guesses]);
+
   // Restarts the game
   const retry = () => {
+    setScore(0);
+    setGuesses(guessesQty);
+
     setGameStage(stages[0].name);
   };
 
@@ -113,6 +131,7 @@ function App() {
           letters={letters}
           guessedLetters={guessedLetters}
           wrongLetters={wrongLetters}
+          guesses={guesses}
           score={score}
         />
       )}
