0557ba10426b578550cb9e51f968da13ad8fb946
Lucas Ramos <lucas.ramos.guimaraes@gmail.com>
2025-07-18 14:13:11 -0300

    Functions and routing with components added

--- secret-word/src/App.jsx
+++ secret-word/src/App.jsx
@@ -1,10 +1,46 @@
-import { useState } from "react";
+// css
 import "./App.css";
+// React
+import { useCallback, useEffect, useState } from "react";
+
+// data
+import { wordsList } from "./data/words";
+
+// Components
+import StartScreen from "./components/StartScreen";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  // Starts the secret word game
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  };
+
+  // Process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  // Restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
+
   return (
     <div className="app">
-      <h2>Secret Word</h2>
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
 }
